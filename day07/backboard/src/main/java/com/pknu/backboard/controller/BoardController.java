4f33adbfd124c82846bd1f75051b3e2e4d21f7b4
dongho <dongho7736@gmail.com>
2025-07-01 17:06:25 +0900

    7일차 커밋

---
+++ day07/backboard/src/main/java/com/pknu/backboard/controller/BoardController.java
@@ -0,0 +1,46 @@
+package com.pknu.backboard.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pknu.backboard.entity.Board;
+import com.pknu.backboard.service.BoardService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/board")
+@Controller
+@Log4j2
+@RequiredArgsConstructor
+public class BoardController {
+
+    @Autowired
+    private final BoardService boardService;
+
+    @GetMapping("/list")
+    public String getList(Model model) {
+        List<Board> boardList = boardService.getBoardList();
+
+        model.addAttribute("boardlist", boardList);
+        return "board_list"; // board_list.html 필요
+    }
+
+    @GetMapping("/detail/{bno}")
+    public String getDetail(Model model, @PathVariable("bno") Long bno) {
+        Board board = this.boardService.getBoardOne(bno);
+
+        model.addAttribute("board", board);
+        return "board_detail";  // board_detail.html 필요
+    }
+    
+
+}
